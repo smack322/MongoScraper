a2be70e81bafa3e0b87d0444724dc3e00edb88e9
Scott <smcanally11@yahoo.com>
2019-02-05 02:57:48 -0500

    setup scraper

--- server.js
+++ server.js
@@ -3,10 +3,37 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const hbhs = require('express-handlebars');
 const axios = require('axios');
-const cherrio = require('cheerio');
+const cheerio = require('cheerio');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+axios.get("http://www.espn.com/espn/latestnews").then(function(response) {
+
+    const $ = cheerio.load(response.data);
+
+    const results = [];
+    console.log($("ul.inline-list.indent"));
+
+    const c = [job1, job2];
+ 
+    $("ul.inline-list.indent").each(function(i, element) {
+
+        console.log("i" + i + "element" + element);
+        const title = $(element).text();
+
+        const link = $(element).parent().attr("href");
+
+        results.push({
+
+            title: title,
+            link: link
+        });
+
+    });
+    console.log(results);
+
+});
 
 app.listen(PORT, function() {
     console.log("Listening on port " + PORT);
