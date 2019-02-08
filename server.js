b77c29ca0055169ed2a8583a0da2c0cf4b21bed0
Scott <smcanally11@yahoo.com>
2019-02-08 11:48:09 -0500

    add models for db, store data from API call into db

--- server.js
+++ server.js
@@ -1,38 +1,57 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-const hbhs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+
+var db = require("./models");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-axios.get("http://www.espn.com/espn/latestnews").then(function(response) {
+axios.get("https://www.philly.com/").then(function(response) {
 
     const $ = cheerio.load(response.data);
 
     const results = [];
-    console.log($("ul.inline-list.indent"));
 
-    const c = [job1, job2];
+    // console.log($("tr td"));
+    // console.log("#threadbits_forum_39");
  
-    $("ul.inline-list.indent").each(function(i, element) {
+    $(".headline-link").each(function(i, element) {
 
-        console.log("i" + i + "element" + element);
-        const title = $(element).text();
+        console.log($(this).children("h4").text());
+        console.log($(this).children("a").attr("href"));
+        
+        results.title = $(this).children("h4").text();
+        results.link = $(this).children("a").attr("href");
 
-        const link = $(element).parent().attr("href");
-
-        results.push({
-
-            title: title,
-            link: link
-        });
-
+        db.Article.create(results)
+          .then(function(dbArticle) {
+              console.log(dbArticle);
+          })
+          .catch(function(error) {
+              console.log(error);
+          });
+        
+        
     });
     console.log(results);
 
+});
+
+//setup handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//setup data transfer 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//setup route for homepage
+app.get('/', function (req, res) {
+    res.render('home');
 });
 
 app.listen(PORT, function() {
