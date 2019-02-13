0c602c4b66fe6fa4ad7f8494415201327bf7ac99
Scott <smcanally11@yahoo.com>
2019-02-13 06:22:58 -0500

    update handlebars

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 
-var db = require("./models");
+const db = require("./models");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/philly_db";
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/philly_db";
 
-mongoose.connect(MONGODB_URI);
+// mongoose.connect(MONGODB_URI);
+
+mongoose.connect("mongodb://localhost/philly_db", { useNewUrlParser: true });
 
 //setup handlebars
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -49,60 +51,66 @@ axios.get("https://www.nytimes.com/section/sports").then(function(response) {
           });    
         
     });
-    res.send("Scrape Complete");
+    res.send("Scraped data has been added to the db");
   });
 });
 
 //setup route for homepage
 app.get('/', function (req, res) {
-    res.render('home');
+  db.Article.find({"home": false}, function(error, data) {
+    const hbsObject = {
+      articles: data
+    };
+    console.log(hbsObject);
+    res.render('home', hbsObject);
+  });
+    
+});
+
+app.get("/", function(req, res) {
+  db.Article.find({"home": true}).populate("notes").exec(function(error, articles) {
+    const hbsObject = {
+      articles: data
+    };
+    res.render("home", hbsObject);
+  });
 });
 
 app.get("/articles", function(req, res) {
     // Grab every document in the Articles collection
     db.Article.find({})
       .then(function(dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
         res.json(dbArticle);
       })
       .catch(function(err) {
-        // If an error occurred, send it to the client
         res.json(err);
       });
   });
 
-  // Route for grabbing a specific Article by id, populate it with it's note
+  // Route get specific article by id (unique identifier), then add notes
 app.get("/articles/:id", function(req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
       .populate("note")
       .then(function(dbArticle) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
         res.json(dbArticle);
       })
       .catch(function(err) {
-        // If an error occurred, send it to the client
         res.json(err);
       });
   });
 
-  // Route for saving/updating an Article's associated Note
+  // Route for save / update each article
 app.post("/articles/:id", function(req, res) {
     // Create a new note and pass the req.body to the entry
     db.Note.create(req.body)
       .then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        //update the specific article based on the users note
         return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
       })
       .then(function(dbArticle) {
-        // If we were able to successfully update an Article, send it back to the client
         res.json(dbArticle);
       })
       .catch(function(err) {
-        // If an error occurred, send it to the client
         res.json(err);
       });
   });
